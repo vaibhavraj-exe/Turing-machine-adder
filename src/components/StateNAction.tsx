56784cf3d1e4b430b26032434af9586e29d1ecbe
vaibhavraj-exe <vraj.exe@gmail.com>
2023-10-21 13:51:22 +0530

    fix: minor bug fix

--- src/components/StateNAction.tsx
+++ src/components/StateNAction.tsx
@@ -10,7 +10,7 @@ const statesAndActions = [
 export default function StateNAction({stateNo} : {stateNo: number}){
     return (
         <div className="flex flex-col items-center">
-            <div className="text-3xl mt-5">State {stateNo} : {statesAndActions[stateNo-1][0]}</div>
+            <div className="text-3xl mt-5">State {stateNo >= 5 ? stateNo-1 : stateNo} : {statesAndActions[stateNo-1][0]}</div>
             <div className="text-2xl mt-2 mb-10">Action: {statesAndActions[stateNo-1][1]}</div>
         </div>
     )
